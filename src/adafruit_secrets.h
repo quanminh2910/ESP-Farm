97f7f5107a20877df2b37e6e316e91900d596e90
thắng <app443325@gmail.com>
2026-03-27 13:07:59 +0700

    ada

--- src/adafruit_secrets.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Wi-Fi
-#define WIFI_SSID     "HCMUT03"
-#define WIFI_PASS     "12characters"
-
-// Adafruit IO
-#define IO_USERNAME   "YOUR_ADAFRUIT_IO_USERNAME"
-#define IO_KEY        "aio_IFuD96zx5PI8QAQvpxQeGlEd0zho"
